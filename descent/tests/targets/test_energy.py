0711a416bf2d69425149ea840565f28d1fb9735a
Joao Morado <joaomorado@gmail.com>
2026-03-23 09:49:19 +0000

    Test energy prediction and dataset creation for periodic systems.

--- descent/tests/targets/test_energy.py
+++ descent/tests/targets/test_energy.py
@@ -35,17 +35,27 @@ def mock_hoh_entry() -> Entry:
     }
 
 
-def test_create_dataset(mock_meoh_entry):
+@pytest.mark.parametrize(
+    "box_vectors",
+    [None, torch.eye(3) * 20.0],
+    ids=["non-periodic", "periodic"],
+)
+def test_create_dataset(mock_meoh_entry, box_vectors):
+    entry = {**mock_meoh_entry, "box_vectors": box_vectors}
+
     expected_entries = [
         {
-            "smiles": mock_meoh_entry["smiles"],
-            "coords": pytest.approx(mock_meoh_entry["coords"].flatten()),
-            "energy": pytest.approx(mock_meoh_entry["energy"]),
-            "forces": pytest.approx(mock_meoh_entry["forces"].flatten()),
+            "smiles": entry["smiles"],
+            "coords": pytest.approx(entry["coords"].flatten()),
+            "energy": pytest.approx(entry["energy"]),
+            "forces": pytest.approx(entry["forces"].flatten()),
+            "box_vectors": None
+            if box_vectors is None
+            else pytest.approx(box_vectors.flatten()),
         },
     ]
 
-    dataset = create_dataset([mock_meoh_entry])
+    dataset = create_dataset([entry])
     assert len(dataset) == 1
 
     entries = list(descent.utils.dataset.iter_dataset(dataset))
@@ -62,11 +72,12 @@ def test_extract_smiles(mock_meoh_entry, mock_hoh_entry):
 
 
 @pytest.mark.parametrize(
-    "reference, normalize,"
+    "box_vectors, reference, normalize,"
     "expected_energy_ref, expected_forces_ref, "
     "expected_energy_pred, expected_forces_pred",
     [
-        (
+        pytest.param(
+            None,
             "mean",
             True,
             torch.tensor([-0.5, 0.5]) / math.sqrt(2.0),
@@ -95,8 +106,10 @@ def test_extract_smiles(mock_meoh_entry, mock_hoh_entry):
                 dtype=torch.float64,
             )
             / math.sqrt(6.0 * 3.0),
+            id="non-periodic-mean-normalized",
         ),
-        (
+        pytest.param(
+            None,
             "min",
             False,
             torch.tensor([0.0, 1.0]),
@@ -123,10 +136,73 @@ def test_extract_smiles(mock_meoh_entry, mock_hoh_entry):
                 ],
                 dtype=torch.float64,
             ),
+            id="non-periodic-min",
+        ),
+        pytest.param(
+            torch.eye(3) * 30.0,
+            "mean",
+            True,
+            torch.tensor([-0.5, 0.5]) / math.sqrt(2.0),
+            torch.tensor(
+                [
+                    [0.0, 1.0, 2.0],
+                    [3.0, 4.0, 5.0],
+                    [6.0, 7.0, 8.0],
+                    [9.0, 10.0, 11.0],
+                    [12.0, 13.0, 14.0],
+                    [15.0, 16.0, 17.0],
+                ],
+                dtype=torch.float64,
+            )
+            / math.sqrt(6.0 * 3.0),
+            torch.tensor([5.585737228393555, -5.585738658905029]),
+            torch.tensor(
+                [
+                    [0.0, -19.695231274883554, 0.0],
+                    [38.04311560258793, 9.847614738308948, 0.0],
+                    [-38.04311560258793, 9.847614738308948, 0.0],
+                    [0.0, 32.39909339680162, 0.0],
+                    [-24.190123962094730, -16.19954490013515, 0.0],
+                    [24.190123962094730, -16.19954490013515, 0.0],
+                ],
+                dtype=torch.float64,
+            ),
+            id="periodic-mean-normalized",
+        ),
+        pytest.param(
+            torch.eye(3) * 30.0,
+            "min",
+            False,
+            torch.tensor([0.0, 1.0]),
+            torch.tensor(
+                [
+                    [0.0, 1.0, 2.0],
+                    [3.0, 4.0, 5.0],
+                    [6.0, 7.0, 8.0],
+                    [9.0, 10.0, 11.0],
+                    [12.0, 13.0, 14.0],
+                    [15.0, 16.0, 17.0],
+                ],
+                dtype=torch.float64,
+            ),
+            torch.tensor([0.0, -15.798852920532227]),
+            torch.tensor(
+                [
+                    [0.0, -83.55978393554688, 0.0],
+                    [161.40325927734375, 41.77988815307617, 0.0],
+                    [-161.40325927734375, 41.77988815307617, 0.0],
+                    [0.0, 137.45770263671875, 0.0],
+                    [-102.62999725341797, -68.72884368896484, 0.0],
+                    [102.62999725341797, -68.72884368896484, 0.0],
+                ],
+                dtype=torch.float64,
+            ),
+            id="periodic-min",
         ),
     ],
 )
 def test_predict(
+    box_vectors,
     reference,
     normalize,
     expected_energy_ref,
@@ -135,7 +211,10 @@ def test_predict(
     expected_forces_pred,
     mock_hoh_entry,
 ):
-    dataset = create_dataset([mock_hoh_entry])
+    entry = {**mock_hoh_entry}
+    if box_vectors is not None:
+        entry["box_vectors"] = box_vectors
+    dataset = create_dataset([entry])
 
     force_field, [topology] = smee.converters.convert_interchange(
         openff.interchange.Interchange.from_smirnoff(
